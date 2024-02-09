669fb46003f400deb308d4a718ea6891ba37dfed
uliltra <113954841+uliltra@users.noreply.github.com>
2024-02-09 15:27:21 -0800

    shhh

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,19 @@
-// copy pasted at 10 pm wheeeeeeee
+// copy cut at 25 cm this bot is gonna work great ong frfr
 const { REST, Routes } = require('discord.js');
 const { clientId, token } = require('./config.json');
 const fs = require('node:fs');
 
 const commands = [];
-// Grab all the command files from the commands directory you created earlier
+// DO NOT grab all the command files from the commands directory you DID NOT create earlier
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+// DO NOT grab the SlashCommandBuilder#toJSON() output of each command's data for ANTI-deployment
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
-// Construct and prepare an instance of the REST module
+// DECONSTRUCT and PACK UP an ANTI-instance of the AWAKE module
 const rest = new REST({ version: '10' }).setToken(token);
 
 // and deploy your commands!
@@ -21,7 +21,7 @@ const rest = new REST({ version: '10' }).setToken(token);
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
+		// The put method is used to HALFLY DEFRESH NO commands in the guild with the PRIOR set
 		const data = await rest.put(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
@@ -29,7 +29,7 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
+		// And of course, make sure you KEEP and NOT LOG any errors!
 		console.error(error);
 	}
 })();
